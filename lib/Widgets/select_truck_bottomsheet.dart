b65f48a31eb595d2f0c13e0f046213042d77de99
ajaop <ajaopaul58@gmail.com>
2023-07-21 22:28:52 +0100

    Working on select time

--- lib/Widgets/select_truck_bottomsheet.dart
+++ lib/Widgets/select_truck_bottomsheet.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:trash_management/CustomExtras/custom_icons_icons.dart';
 
@@ -48,51 +49,92 @@ class select_truck_bottomsheet extends StatelessWidget {
               child: ListView(
                 shrinkWrap: true,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Color(0xffEEF7F0),
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                      ),
-                      padding: EdgeInsets.all(10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          ImageIcon(
-                            AssetImage("images/garbage_truck1.png"),
-                            size: 50.0,
-                            color: Color(0xff08110B),
-                          ),
-                          Column(
-                            children: [
-                              Text('Mini Waste Truck',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyLarge!
-                                      .copyWith(
-                                          fontSize: 19.0,
-                                          color: Color(0xff08110B),
-                                          fontWeight: FontWeight.bold)),
-                              Text('12kg - 17kg',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyLarge!
-                                      .copyWith(
-                                          fontSize: 17.0,
-                                          color: Color(0xff08110B),
-                                          fontWeight: FontWeight.normal))
-                            ],
-                          ),
-                          Text('₦ 1500',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyLarge!
-                                  .copyWith(
-                                      fontSize: 19.0,
-                                      color: Color(0xff08110B),
-                                      fontWeight: FontWeight.w800))
-                        ],
+                  InkWell(
+                    onTap: () {
+                      showModalBottomSheet(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return Container(
+                              color: Colors.white,
+                              height: screenHeight * 0.42,
+                              child: Column(
+                                children: [
+                                  CupertinoDatePicker(
+                                    minimumDate:
+                                        DateTime.now().add(Duration(days: 1)),
+                                    initialDateTime:
+                                        DateTime.now().add(Duration(days: 1)),
+                                    onDateTimeChanged: (DateTime newdate) {
+                                      print(newdate);
+                                    },
+                                    mode: CupertinoDatePickerMode.date,
+                                  ),
+                                  Expanded(
+                                    child: ElevatedButton(
+                                        style: ElevatedButton.styleFrom(
+                                            primary: Color(0xff19433C),
+                                            maximumSize:
+                                                const Size.fromHeight(55),
+                                            textStyle: Theme.of(context)
+                                                .textTheme
+                                                .button!
+                                                .copyWith(
+                                                    fontSize: 20.0,
+                                                    color: Colors.white)),
+                                        onPressed: () {},
+                                        child: Text('Save')),
+                                  )
+                                ],
+                              ),
+                            );
+                          });
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Color(0xffEEF7F0),
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                        padding: EdgeInsets.all(10.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            ImageIcon(
+                              AssetImage("images/garbage_truck1.png"),
+                              size: 50.0,
+                              color: Color(0xff08110B),
+                            ),
+                            Column(
+                              children: [
+                                Text('Mini Waste Truck',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodyLarge!
+                                        .copyWith(
+                                            fontSize: 19.0,
+                                            color: Color(0xff08110B),
+                                            fontWeight: FontWeight.bold)),
+                                Text('12kg - 17kg',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodyLarge!
+                                        .copyWith(
+                                            fontSize: 17.0,
+                                            color: Color(0xff08110B),
+                                            fontWeight: FontWeight.normal))
+                              ],
+                            ),
+                            Text('₦ 1500',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyLarge!
+                                    .copyWith(
+                                        fontSize: 19.0,
+                                        color: Color(0xff08110B),
+                                        fontWeight: FontWeight.w800))
+                          ],
+                        ),
                       ),
                     ),
                   ),
